bd4fc6a5d20b98eb7183c6eb1194446007ef102e
have-no-clue-what-im-doing <brodericjduncan@gmail.com>
2023-10-09 21:54:30 -0400

    Project 1
    Auto dashcam video merger

---
+++ Project_1/Project01.py
@@ -0,0 +1,40 @@
+import os
+import ffmpeg
+
+path = "E:\\yikes\\BlackVue\\Record"
+
+videoList = os.listdir(path)
+
+def OrganizeVideo():
+    rejects = []
+    frontView = []
+    rearView = []
+    for video in videoList:
+        if "PR" in video:
+            rejects.append(video)
+        elif "PF" in video:
+            rejects.append(video)    
+        elif "R" in video:
+            rearView.append(video)
+        elif "F" in video:
+            frontView.append(video)
+    with open("video_front.txt", "w") as f:
+        for file in frontView:
+            f.write(f"file '{file}'")
+            f.write("\n")
+        f.close()
+        os.system("move video_front.txt E:\\yikes\\BlackVue\\Record")
+    with open("video_rear.txt", "w") as f:
+        for file in rearView:
+            f.write(f"file '{file}'")
+            f.write("\n")
+        f.close()    
+        os.system("move video_rear.txt E:\\yikes\\BlackVue\\Record")
+    os.system("E:")
+    os.system("cd E:\\yikes\\BlackVue\\Record")
+    
+    ffmpeg.input('video_rear.txt', format='concat', safe=0).output('output.mkv', c='copy').run()
+    ffmpeg.input('video_front.txt', format='concat', safe=0).output('output.mkv', c='copy').run()
+            
+OrganizeVideo()
+    
