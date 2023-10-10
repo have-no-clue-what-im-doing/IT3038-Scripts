fe2cafef9c9ea4994a82d9090cbefb4c78b17a4d
have-no-clue-what-im-doing <brodericjduncan@gmail.com>
2023-10-09 21:57:36 -0400

    update some stuff

--- Project_1/Project01.py
+++ Project_1/Project01.py
@@ -33,8 +33,8 @@ def OrganizeVideo():
     os.system("E:")
     os.system("cd E:\\yikes\\BlackVue\\Record")
     
-    ffmpeg.input('video_rear.txt', format='concat', safe=0).output('output.mkv', c='copy').run()
-    ffmpeg.input('video_front.txt', format='concat', safe=0).output('output.mkv', c='copy').run()
+    ffmpeg.input('video_rear.txt', format='concat', safe=0).output('rear.mkv', c='copy').run()
+    ffmpeg.input('video_front.txt', format='concat', safe=0).output('front.mkv', c='copy').run()
             
 OrganizeVideo()
     
