51dfc4758b76bbd6b42ae81b675363b80cf540f5
have-no-clue-what-im-doing <brodericjduncan@gmail.com>
2023-10-15 11:49:34 -0400

    Lab 7

---
+++ Labs/Lab07/MusicToFallAsleepTo.py
@@ -0,0 +1,33 @@
+import winsound
+import random
+
+def MusicPlayer(freq):
+    winsound.Beep(freq, 500)
+    
+def FreqChanger():
+    freq = 37
+    while True:
+        if freq >= 12000:
+            freq = 37
+        else:
+            freq += 100
+        print(freq)
+        MusicPlayer(freq)
+
+FreqChanger()
+
+def GoingDeaf():
+    while True:
+        winsound.Beep(10000, 100000)
+
+#GoingDeaf()
+
+def PitchPerfect():
+    while True:
+        randomFreq = random.randrange(37, 10000)
+        randomTime = random.randrange(10, 1000)
+        winsound.Beep(randomFreq, randomTime)
+        PitchPerfect()
+
+PitchPerfect()
+
