9963bdea7215760568261df867454981902fd172
have-no-clue-what-im-doing <brodericjduncan@gmail.com>
2023-11-18 21:16:41 -0500

    lab06

---
+++ Labs/Lab06/test.js
@@ -0,0 +1,8 @@
+var path = require("path");
+
+
+console.log("Using PATH module:");
+console.log(`Hello from file ${path.basename(__filename)}`);
+
+console.log(`Process args: ${process.argv}`)
+console.log(`Process args: ${process.argv}`)
