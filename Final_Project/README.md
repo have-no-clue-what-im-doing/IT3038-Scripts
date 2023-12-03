ec552dff9520d30d8af2a07907ae758e26202795
have-no-clue-what-im-doing <brodericjduncan@gmail.com>
2023-12-02 21:17:47 -0500

    Final Project Submission

---
+++ Final_Project/README.md
@@ -0,0 +1,51 @@
+# Requirements and Dependencies:
+
+This script checks to see if there is Newcastle United FC game for the day, and if there is, power on a remote computer and display the game.
+
+There are also two versions of this script for either Windows or Debian based Linux
+To test this script you will need the following:
+
+1 Windows computer to display the Newcastle game (as host)
+1 Windows / Linux computer to run the script (as client)
+
+The host machine must have a NIC that allows wake on lan. Otherwise you will have to manually turn on the machine yourself before running the script
+
+Both machines will need `SSH`
+For most Linux distros it should be installed by default. If not you can run `sudo apt-get install openssh-client`
+For windows machines you will need to install [OpenSSH](https://learn.microsoft.com/en-us/windows-server/administration/openssh/openssh_install_firstuse?tabs=powershell) 
+Be sure to add firewall rules to allow SSH traffic.
+
+For the Windows host machine, you need to enable [auto-login](https://learn.microsoft.com/en-us/troubleshoot/windows-server/user-profiles-and-logon/turn-on-automatic-logon)
+
+Be sure to make note of the following on your host machine:
+
+`Username`
+`Password`
+`IP Address`
+`MAC Address`
+
+
+For the following functions you will have to add in your own login and machine info:
+`PowerOnComputer`
+`RestartComputer`
+`CreateChromeShortcut`
+
+Yes, having the password in plain text is bad practice, but this has already taken up enough of my time
+
+Also ensure both machines are on the same broadcast domain so wake on lan works
+
+You'll also need Peacock and YoutubeTV. Technically not if you just want to see if it work without watching the game.
+
+#Installation
+
+This script runs off of Python and Powershell. Your host machine must have Powershell and your client machine must have both Python and Powershell
+[Link to download Python](https://www.python.org/downloads/)
+Powershell should be downloaded by default on Windows unless you disabled / uninstalled it
+
+Before running the script, be sure to install all dependencies:
+`pip install -r requirments.txt` 
+
+If you're running the script off Linux you will also need wakeonlan, which can be installed here:
+`apt-get install wakeonlan`
+
+Once all requirements are downloaded and installed you are good to run the script. Feel free to have this run via task scheduler or as a cron job. Best time would be in morning, a couple hours before 9am local time. 
---
+++ Final_Project/Windows/NewcastleUnitedFinal.py
@@ -0,0 +1,250 @@
+import requests
+import json
+import time
+from datetime import datetime, timezone, timedelta
+from bs4 import BeautifulSoup
+import paramiko
+import subprocess
+
+#Make a request to API to get Newcastle United schedule and return list.
+def GetFixtures():
+    url = "https://api.football-data.org/v4/teams/67/matches"
+    token = "53e06e2ba29e4f9aac750911b6d870d7"
+    headers = {
+    'X-Auth-Token': token,
+    'Content-Type': 'application/json',
+    }
+    response = requests.get(url, headers=headers)
+    data = json.loads(response.text)
+    matchData = data["matches"]
+    return matchData
+
+#Get current time in UTC and format it so it can be compared to the time format that is returned in the GetFixtures matchData.
+def GetCurrentTimeUTC():
+    currentTimeUTC = datetime.now(timezone.utc)
+    formatTime = currentTimeUTC.strftime("%Y-%m-%dT%H:%M:%SZ")
+    return formatTime
+
+#Get just the year, month, and day.
+def GetCurrentDate():
+    currentTimeUTC = datetime.now(timezone.utc)
+    formatDate = currentTimeUTC.strftime("%Y-%m-%d")
+    return formatDate
+
+#Convert the current time to UTC and format it.
+def ConvertUnixTimeToUTC(time):
+    unixInSeconds = time / 1000
+    utcTime = datetime.utcfromtimestamp(unixInSeconds).replace(tzinfo=timezone.utc)
+    utcTimeFormatted = utcTime.strftime('%Y-%m-%dT%H:%M:%SZ')
+    return utcTimeFormatted
+
+#Make API request to see if there is a match on this current day. If there is, confirm it is a Premier League game. 
+#(Ignore other games like Champions League) If there is a game today, then return a dict of just the data for that game.
+def IsItMatchDay():
+    matches = GetFixtures()
+    for match in matches:
+        if match["competition"]["name"] == "Premier League":
+            currentDate = GetCurrentDate()
+            matchDate = match["utcDate"]
+            if currentDate in matchDate:
+                return matchDate
+    return "No matches today"
+
+#Make a search using the term "Newcastle v" to list recent upcoming games. Return list of Newcastle games.
+#I copied this request using Chrome dev tools. This is something that has a high chance of breaking in the future if Peacock changes anything.
+def PeacockRequest():
+   headers = {
+    'authority': 'web.clients.peacocktv.com',
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'no-cache',
+    'origin': 'https://www.peacocktv.com',
+    'pragma': 'no-cache',
+    'referer': 'https://www.peacocktv.com/',
+    'sec-ch-ua': '"Google Chrome";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36',
+    'x-skyott-ab-recs': 'SearchExtensionV2:control;fylength:variation2',
+    'x-skyott-activeterritory': 'US',
+    'x-skyott-bouquetid': '5566233252361580117',
+    'x-skyott-client-version': '4.11.23',
+    'x-skyott-device': 'COMPUTER',
+    'x-skyott-language': 'en',
+    'x-skyott-platform': 'PC',
+    'x-skyott-proposition': 'NBCUOTT',
+    'x-skyott-provider': 'NBCU',
+    'x-skyott-subbouquetid': '0',
+    'x-skyott-territory': 'US',
+    }
+   params = {
+    'term': 'newcastle v',
+    'limit': '40',
+    'entityType': 'programme,series',
+    'contentFormat': 'longform',
+    }
+   response = requests.get('https://web.clients.peacocktv.com/bff/search/v2', params=params, headers=headers)
+   data = json.loads(response.text)
+   return data
+    
+#Iterate through list to see if the matchDate from IsItMatchDay() matches with a game on Peacock. (Peacock sets start time 10 minutes before match start)
+#Add 10 minutes to time to compare
+#Return the specific link to that game if there is a match
+def SearchPeacock(gameTime):
+    data = PeacockRequest()
+    matches = data["data"]["search"]["results"]
+    tenMinutesUnix = (10 * 60 * 1000)
+    for match in matches:
+        startTime = match["displayStartTime"]
+        time = ConvertUnixTimeToUTC(startTime + tenMinutesUnix)
+        if gameTime == time:
+            gameLink = match["slug"]
+            return "https://peacocktv.com" + gameLink
+    return "Error: No matches found"
+
+#First check to see if game is on Peacock, if not, web scrape TVInsider to see if game is on NBC or USA.
+def GetStreamingLink():
+    confirmMatch = IsItMatchDay()
+    if confirmMatch == "No matches today":
+        return "No matches today"
+    else:
+        confirmPeacock = SearchPeacock(confirmMatch)
+        if confirmPeacock == "Error: No matches found":
+            return SearchYoutubeTV()
+        else:
+            return confirmPeacock
+
+#Scrape webpage and return list of EPL games scheduled for the week.
+def GetTVProviderData():
+    url = "https://www.tvinsider.com/show/premier-league-soccer/"
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Accept-Language': 'en-US,en;q=0.5',
+    }
+    r = requests.get(url, headers=headers)
+    s = BeautifulSoup(r.content, "html5lib")
+    gamesList = s.find("div", class_="games")
+    games = gamesList.find_all("div", class_="game")
+    return games
+
+
+#Find Newcastle's game and determine whether game is on NBC or USA. 
+def FindTVProvider():
+    games = GetTVProviderData()
+    for game in games:
+        if "Newcastle" in game.find("h4").text:
+            if "USA Network" in game.find("h5").text:
+                return "USA"
+            else:
+                return "NBC"
+    return "Error, unable to find a Newcastle game for this week"
+
+#If USA, return youtube.tv USA link, and if NBC, return youtube.tv NBC link
+def SearchYoutubeTV():
+    tvNetwork = FindTVProvider()
+    if tvNetwork == "USA":
+        return "https://tv.youtube.com/watch/M2YyNsA47Lw?utm_servlet=prod&rd_rsn=asi&zipcode=45103&onboard=1&vp=0gEEEgIwAQ%3D%3D"
+    if tvNetwork == "NBC":
+        return "https://tv.youtube.com/watch/by5X_xkztY8?utm_servlet=prod&rd_rsn=asi&zipcode=45103&onboard=1&vp=0gEEEgIwAQ%3D%3D"
+    else:
+        return "Error, unable to find YoutubeTV provider"
+
+#Calculate time 15 minutes before start of match     
+def GetComputerStartTime():
+    matchTime = IsItMatchDay()
+    matchObj = datetime.strptime(matchTime, "%Y-%m-%dT%H:%M:%SZ")
+    newMatchObj = matchObj - timedelta(minutes=15)
+    matchTimeStr = newMatchObj.strftime("%Y-%m-%dT%H:%M:%SZ")
+    return matchTimeStr
+
+#Return difference between match start time and computer start time in seconds to use for time.sleep(). 
+#We want to wait until 15 minutes before game to turn on computer
+def GetSleepTime():
+    startTime = GetComputerStartTime()
+    convertStartTime = datetime.strptime(startTime, "%Y-%m-%dT%H:%M:%SZ")
+    currentTime = datetime.utcnow()
+    timeDiff = convertStartTime - currentTime
+    timeDiffSecs = timeDiff.total_seconds()
+    return timeDiffSecs
+
+#Credit: https://stackoverflow.com/questions/72853502/how-to-send-a-wake-on-lan-magic-packet-using-powershell
+def PowerOnComputer(mac):
+    wakeOnLanCommand = f''' 
+    $mac = '{mac}';
+    [System.Net.NetworkInformation.NetworkInterface]::GetAllNetworkInterfaces() | Where-Object {{ $_.NetworkInterfaceType -ne [System.Net.NetworkInformation.NetworkInterfaceType]::Loopback -and $_.OperationalStatus -eq [System.Net.NetworkInformation.OperationalStatus]::Up }} | ForEach-Object {{
+    $networkInterface = $_
+    $localIpAddress = ($networkInterface.GetIPProperties().UnicastAddresses | Where-Object {{ $_.Address.AddressFamily -eq [System.Net.Sockets.AddressFamily]::InterNetwork }})[0].Address
+    $targetPhysicalAddress = [System.Net.NetworkInformation.PhysicalAddress]::Parse(($mac.ToUpper() -replace '[^0-9A-F]',''))
+    $targetPhysicalAddressBytes = $targetPhysicalAddress.GetAddressBytes()
+    $packet = [byte[]](,0xFF * 102)
+    6..101 | Foreach-Object {{ $packet[$_] = $targetPhysicalAddressBytes[($_ % 6)] }}
+    $localEndpoint = [System.Net.IPEndPoint]::new($localIpAddress, 0)
+    $targetEndpoint = [System.Net.IPEndPoint]::new([System.Net.IPAddress]::Broadcast, 9)
+    $client = [System.Net.Sockets.UdpClient]::new($localEndpoint)
+    try {{ $client.Send($packet, $packet.Length, $targetEndpoint) | Out-Null }} finally {{ $client.Dispose() }}
+    }}
+    '''
+    subprocess.run(['powershell', '-Command', wakeOnLanCommand], capture_output=True, text=True)
+    return "Computer Powered On"
+
+#Connect to remote computer via ssh and send a restart command
+def RestartComputer():
+    hostname = '192.168.1.114'
+    port = 22
+    username = 'Broderic'
+    password = 'newcastle123'
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname, port, username, password)
+    ssh.exec_command("shutdown /f /r /t 0")
+    ssh.close()
+    return "Computer has been restarted! Game is ready to watch!"
+
+#Create a Chrome shortcut with the match link in the Windows Startup Folder using Powershell. 
+def CreateChromeShortcut(link):
+    hostname = '192.168.1.114'
+    port = 22
+    username = 'broderic'
+    password = 'newcastle123'
+    powershellCommands = f'''
+    $chromePath = "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+    $targetURL = "{link}";
+    $shortcutPath = "C:\\Users\\Broderic\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\\Newcastle.lnk";
+    $wshShell = New-Object -ComObject WScript.Shell;
+    $shortcut = $wshShell.CreateShortcut($shortcutPath);
+    $shortcut.TargetPath = $chromePath;
+    $shortcut.Arguments = "$targetURL --start-fullscreen";
+    $shortcut.Description = "Google Chrome - Fullscreen";
+    $shortcut.IconLocation = "$chromePath,0";
+    $shortcut.WorkingDirectory = "C:\\Program Files\\Google\\Chrome\\Application";
+    $shortcut.Save();
+    '''
+    ssh = paramiko.SSHClient()
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh.connect(hostname, port, username, password)
+    ssh.exec_command(powershellCommands)
+    ssh.close()
+    print(f"Created Chrome Shortcut to this url: {link}")
+    return "Created Chrome Shortcut"
+
+#Determine if there is a match today, and if there is, find the link for the proper network and run it on the remote computer.
+def WatchNewcastleMatch():
+    getMatchLink = GetStreamingLink()
+    if getMatchLink == "No matches today":
+        return "No matches today"
+    else: 
+        waitForMatch = GetSleepTime()
+        time.sleep(waitForMatch)
+        matchLink = GetStreamingLink()
+        PowerOnComputer("d8:bb:c1:0d:25:bb")
+        time.sleep(120)
+        CreateChromeShortcut(matchLink)
+        time.sleep(15)
+        RestartComputer()
+        return "All commands sent successfully, game is ready to watch!"
+
+print(WatchNewcastleMatch())
+
